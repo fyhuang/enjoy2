d298f6751a2138d740bb98c2886e5cdbe206843c
Jonas Maebe <jonas.maebe@elis.ugent.be>
2013-11-20 22:01:08 +0100

    The inSender parameter of IOHIDValueCallback (input_callback) is already a HID device.
    This change fixes the detection of joystick events on Mac OS X 10.6. On 10.9 the
    old code also worked, presumably because IOHIDQueueGetDevice returned the input
    parameter as result in case it wasn't an IOHIDQueueRef (on 10.6 it returned
    NULL)

--- JoystickController.m
+++ JoystickController.m
@@ -62,7 +62,7 @@ void timer_callback(CFRunLoopTimerRef timer, void *ctx) {
 
 void input_callback(void* inContext, IOReturn inResult, void* inSender, IOHIDValueRef value) {
 	JoystickController* self = (JoystickController*)inContext;
-	IOHIDDeviceRef device = IOHIDQueueGetDevice((IOHIDQueueRef) inSender);
+	IOHIDDeviceRef device = (IOHIDQueueRef) inSender;
 	
 	Joystick* js = [self findJoystickByRef: device];
     ApplicationController *app_controller = [[NSApplication sharedApplication] delegate];
